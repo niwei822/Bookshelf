4f67c7008f4f74edecf0715f86a8b4192258d91d
Niwei <cecily.li.822@gmail.com>
2022-07-07 16:14:24 -0700

    Initial Commit

---
+++ BookShelfApp/BookShelfAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookShelfAppApp.swift
+//  BookShelfApp
+//
+//  Created by cecily li on 7/7/22.
+//
+
+import SwiftUI
+
+@main
+struct BookShelfAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
